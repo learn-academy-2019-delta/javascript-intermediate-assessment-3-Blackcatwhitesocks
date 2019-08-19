6a0fd320a36c23d879fcb829afdeae9dadb0f942
Ubuntu <ubuntu@ip-172-31-45-223.us-east-2.compute.internal>
2019-08-19 06:48:27 +0000

    completed assessment

--- js-intermediate.js
+++ js-intermediate.js
@@ -5,101 +5,242 @@
 
 // 1. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-
+// function getFib(){
+//   let arr = [1]
+//   let last2 = 0
+//   let last1 = 1
+//   for (var i = 0; i<9; i++){
+//   let sum = last2 + last1
+//   arr.push(sum)
+//   last2 = last1
+//   last1 = sum
+//   }
+// return arr
+// }
+// console.log(getFib());
 
 // 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+// var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+
+// function oddChecker(arr){
+//   let oddArr = []
+//   for (i = 0; i < arr.length; i++){
+//     if(typeof arr[i] == "number"){
+//       if( Math.abs(arr[i])%2 === 1){
+//         oddArr.push(arr[i])
+//       }
+//     }
+//   }
+//   return oddArr
+// }
+// console.log(oddChecker(fullArr1));
+
 // Expected output: [9, 7, 199, -9]
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+
+// var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+
+// function oddChecker(arr){
+//   let oddArr = []
+//   for (i = 0; i < arr.length; i++){
+//     if(typeof arr[i] == "number"){
+//       if( Math.abs(arr[i])%2 === 1){
+//         oddArr.push(arr[i])
+//       }
+//     }
+//   }
+//   return oddArr
+// }
+// console.log(oddChecker(fullArr2));
 // Expected output: [7, 23, -823]
 
 
 
 // 3. Given the object below, complete the console.log to find specific information:
 
-var bicycle = {
-	type: "Roadbike",
-	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
-	wheels: {
-		count: 2,
-		specs: ["road tires", "AX-7563", "80-115 PSI"],
-		brand: "Trek"
-	}
-}
+// var bicycle = {
+// 	type: "Roadbike",
+// 	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+// 	wheels: {
+// 		count: 2,
+// 		specs: ["road tires", "AX-7563", "80-115 PSI"],
+// 		brand: "Trek"
+// 	}
+// }
 
 // Log the type of bicycle:
-console.log()
+// console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+// console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+// console.log(bicycle.wheels.specs[2])
 
 
 // 4. Write a function that takes one argument of type array and returns the contents of that array in reverse order.
 
-var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
+// var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
+
+// function Reverse(arr){
+//   return arr.reverse()
+// }
+// console.log(Reverse(originalArray1));
+
 // Expected output: [7, 6, 5, 4, 3, 2, 1],
-var originalArray2 = ["a", "t", "l", "e", "d"]
+
+// var originalArray2 = ["a", "t", "l", "e", "d"]
+
+// function Reverse(arr){
+//   return arr.reverse()
+// }
+// console.log(Reverse(originalArray2));
+
 // Expected output: ["d", "e", "l", "t", "a"]
 
 
 
 // 5. Write the code that would make this test pass.
 
-describe("multTwo", () => {
-    test ("returns an array with all the numbers multiplied by two", () => {
-        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
-        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
-        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
-    })
-})
+// describe("multTwo", () => {
+//     test ("returns an array with all the numbers multiplied by two", () => {
+//         expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+//         expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+//         expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+//     })
+// })
+// / const multTwo = arr1.map(x => {
+//   console.log(x*2)
+// });
 
+// arr1.forEach(function(x) {
+//   console.log(x*2)
+// })
 
 
 // 6. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
-var ourString = "Hello Learn Students!"
+// var ourString = "Hello Learn Students!"
 // Expected output: 3
 
+// / function letterCounter(str){
+//   let counter = 0
+//   for (var i=0; i < str.length; i++){
+//     if( str.charAt(i) === 'l' || str.charAt(i) === 'L'){
+//       counter += 1
+//     }
+//   }
+//   return counter
+// }
+// console.log(letterCounter(ourString));
 
 
 // 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
 
-var middleLetters1 = "hello"
+// var middleLetters1 = "hello"
+
+// function middleLetters(str){
+//     if ( str.length%2 === 1){
+//       return str.charAt((str.length)/2)
+//     } else {
+//      return str.substring((str.length)/2-1, (str.length)/2+1)
+//    }
+// }
+// console.log(middleLetters(middleLetters1));
+
 // Expected output: “l”
-var middleLetters2 = "lamas"
-// Expected output: “am”
+/
+
+// function middleLetters(str){
+//
+//     if ( str.length%2 === 1){
+//       return str.charAt((str.length)/2)
+//     } else {
+//      return str.substring((str.length)/2-1, (str.length)/2+1)
+//    }
+// }
+// console.log(middleLetters(middleLetters2));
+
+// Expected output: “am” - looks like a typo here, "lamas" is odd, should return 1 letter 
 
 
 
 
 // 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
-
+// class sphere{
+//   constructor(radius){
+//     this.radius = radius;
+//   }
+//   calcArea (){
+//   return  4*Math.PI*Math.pow(this.radius, 2)
+//   }
+// }
+// let sphere1 = new sphere(2)
+// let sphere2 = new sphere(3)
+// let sphere3 = new sphere(7)
+//
+// console.log(sphere1.calcArea());
+// console.log(sphere2.calcArea());
+// console.log(sphere3.calcArea());
 
 
 // 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
 // Don't overthink this one, it's just destrcturing practice.
 
-var myCar = {
-  make: "VW",
-  model: "Jetta",
-  specs: {
-    doors: 4,
-    transmission: "manual",
-  }
-}
-
+// var myCar = {
+//   make: "VW",
+//   model: "Jetta",
+//   specs: {
+//     doors: 4,
+//     transmission: "manual",
+//   }
+// }
+// console.log(myCar.specs.doors, myCar.specs.transmission);
 
 
 // 10. Stretch: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
-var numbersToAdd1 = [2, 4, 45, 9]
+// var numbersToAdd1 = [2, 4, 45, 9]
+
+// function numbersToAdd(arr){
+//   let accumulator = 0
+//   let newArr = []
+//   for (var i=0; i < arr.length; i++){
+//   accumulator = accumulator + arr[i]
+//   newArr.push(accumulator)
+//   }
+// return newArr
+// }
+// console.log(numbersToAdd(numbersToAdd1));
+
 // Excpected output: [2, 6, 51, 60]
-var numbersToAdd2 = [0, 7, -8, 12]
+
+// var numbersToAdd2 = [0, 7, -8, 12]
+// function numbersToAdd(arr){
+//   let accumulator = 0
+//   let newArr = []
+//   for (var i=0; i < arr.length; i++){
+//   accumulator = accumulator + arr[i]
+//   newArr.push(accumulator)
+//   }
+// return newArr
+// }
+// console.log(numbersToAdd(numbersToAdd2));
+
 // Expected output: [0, 7, -1, 11]
-var numbersToAdd3 = []
+
+// var numbersToAdd3 = []
+
+// function numbersToAdd(arr){
+//   let accumulator = 0
+//   let newArr = []
+//   for (var i=0; i < arr.length; i++){
+//   accumulator = accumulator + arr[i]
+//   newArr.push(accumulator)
+//   }
+// return newArr
+// }
+// console.log(numbersToAdd(numbersToAdd3));
+
 // Expected output: []
--- js-intermediate.md
+++ js-intermediate.md
@@ -5,36 +5,42 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What are the HTTP verbs? Which CRUD action corresponds to each verb?
 
-  Your answer:
+  Your answer:: Create, Read, Update, Delete.
 
-  Researched answer:
+  Researched answer:The HTTP verbs comprise a major portion of our “uniform interface” constraint and provide us the action counterpart to the noun-based resource. The primary or most-commonly-used HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, PATCH, and DELETE. These correspond to create, read, update, and delete (or CRUD) operations, respectively. There are a number of other verbs, too, but are utilized less frequently. Of those less-frequent methods, OPTIONS and HEAD are used more often than others.
 
 
 2. Why would you use object destructuring?
 
-  Your answer:
+  Your answer:Destructuring is a part of ES6. It's an expression that allows to extract from objects, arrays or set of objects.
 
-  Researched answer:
+  Researched answer:The destructuring assignment  syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
 
 
 
 3. Why is testing important?
 
-  Your answer:
+  Your answer:It’s important to test the code because it helps to find code bugs easier. 
 
-  Researched answer:
+  Researched answer:•	Software testing and code review spreads knowledge across the entire team. No one  person is responsible for proper coding — which, in the long run, relieves stress. This doesn’t give you, the developer, the right to slack off, or not complete your job on time, but it does give you peace of mind to know that your work will be checked, and double-checked, for maximum efficiency.
+•	Software testing and code review fits right in with the existing team’s flow of process. By doing it this way, the software testing and code review team can make sure that the product is working perfectly before it’s implemented.
+•	Even though, on the surface, it seems as though code review takes time (and it does), it also saves time in the long-term. Reason being: when the code works properly the first time, it doesn’t have to be deactivated or re-tooled to work properly the second, third, and fourth time. In addition, this type of review creates an opportunity for growth and empowerment amongst junior engineers.
+
 
 
 4. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer:Class consists of the properties that belong to the object. 
 
-  Researched answer:
+  Researched answer:Class is like a blueprint description for the object to be created. Classes inherit from classes and create subclass relationships: hierarchical class taxonomies. Objects inherit directly from other objects. 
 
 
 5. What did you learn during the group project this week? Please include any additional feedback you may have.
 
   Your answer:
+During the group project I learned that I have more blind spots than I thought I did and need to spend more time on rehearsing class material at home.  
+
 
 
 
@@ -45,34 +51,43 @@ Answer the following questions. First, without external resources. Challenge you
   Your answer:
 
   Researched answer:
-
+The external Style Sheet (.css file) always seperate from HTML file. You can linkthis external file (.css file) to your HTML document file using the < link > tag . You can place this < link > tag Within the < head > section, and after the < title > element of your HTML file.
 
 2. What is the difference between a div and a span?
 
   Your answer:
 
   Researched answer:
+ The difference between span and div is that a span element is in-line and usually used for a small chunk of HTML inside a line (such as inside a paragraph) whereas a div (division) element is block-line (which is basically equivalent to having a line-break before and after it) and used to group larger chunks of code.
+
 
 
 3. What is a CSS class? When should you use an id instead of a class?
 
   Your answer:
 
-  Researched answer:
+  Researched answer:The .class selector selects elements with a specific class attribute.
+To select elements with a specific class, write a period (.) character, followed by the name of the class.
+
 
 
 4. Name 4 semantic HTML tags.
 
   Your answer:
 
-  Researched answer:
+  Researched answer:Semantic HTML elements clearly describe it’s meaning in a human and machine readable way. Elements such as <header>, <footer> and <article> are all considered semantic because they accurately describe the purpose of the element and the type of content that is inside them.
 
 
 5. What are three options for creating responsive design?
 
   Your answer:
 
-  Researched answer:
+  Researched answer:1) Media queries let you design different layouts for different media types like screen, print, TV, handheld devices etc. Depending on the media type, you can configure the style, the font and other elements of the page.
+Introduced in CSS3, media queries let a designer customize the presentation of the content to suit a specific range of output device types. They consist of a media type declaration and one or more media feature expressions that evaluate to true or false.
+2) “Fluid grid” is a term used to describe a layout that is coded in relative proportions of the various elements, rather than with fixed pixel values.
+Traditional CSS layouts used fixed width grids to position the elements. The fixed-width approach no longer works today now that we have device screens ranging from 3 inches to a few feet. In a fluid grid design, everything is coded in terms of its relative proportion to the base element i.e. instead of specifying the height and width of each element; it is given a percentage or relative size. 
+3) Images are deemed flexible when the image renders in native size, as long as the HTML container supports it, but when the browser window shrinks, the image scales to fit it.
+
 
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
@@ -81,18 +96,18 @@ Answer the following questions. First, without external resources. Challenge you
 
   Your answer:
 
-  Researched answer:
+  Researched answer:Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application so that a user can see and interact with them directly. The challenge associated with front end development is that the tools and techniques used to create the front end of a website change constantly and so the developer needs to constantly be aware of how the field is developing.
 
 
 2. What is block scope in JavaScript?
 
   Your answer:
 
-  Researched answer:
+  Researched answer:A block scope is the area within if, switch conditions or for and while loops.
 
 
 3. How would you explain the idea of "inheritance" in object oriented programming?
 
   Your answer:
 
-  Researched answer:
+  Researched answer:Inheritance is a mechanism in which one class acquires the property of another class. For example, a child inherits the traits of his/her parents. With inheritance, we can reuse the fields and methods of the existing class. Hence, inheritance facilitates Reusability and is an important concept of OOPs.
